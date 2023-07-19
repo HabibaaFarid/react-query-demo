f9f0f6bbaa85524822d18686a4140a29d520a162
Habiba <habiibafarid@gmail.com>
2023-07-19 12:29:34 +0300

    useQuery and useMutation

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import PostsList1 from "./PostsList1";
+import PostsList2 from "./PostsList2";
+import Post from "./Post";
+import { CreatePost } from "./CreatePost";
+
+function App(){
+  const [currentPage, setCurrentPage] = useState(<PostsList1/>)
+
+  return(
+    <div>
+      <button onClick={()=> setCurrentPage(<PostsList1/>)}>Posts List 1</button>
+      <button onClick={()=> setCurrentPage(<PostsList2/>)}>Posts List 2</button>
+      <button onClick={()=> setCurrentPage(<Post id={1}/>)}>First Post</button>
+      <button onClick={()=> setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)}>Create Post</button>
+      <br />
+      {currentPage}
+    </div>
+  )
+}
+
+export default App;
